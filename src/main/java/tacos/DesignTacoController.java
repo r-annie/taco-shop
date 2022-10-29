00bacd4893a57c043ce85d4c2645012e8ed4380b
Anna Tarasova <atarasova@dcloud.tech>
2022-10-29 17:37:19 +0400

    feat: added order controller, form and id-to-ingredient controller

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import tacos.Ingredient.Type;
@@ -56,7 +57,15 @@ public class DesignTacoController {
         return "design";
     }
 
-    private Iterable<Ingredient> filterByType (List<Ingredient> ingredients, Type type) {
+    @PostMapping
+    public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+
+        return "redirect:/orders/current";
+    }
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
