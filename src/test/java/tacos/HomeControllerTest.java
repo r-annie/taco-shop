4f663d71b087f258fe4668df8a1f6ce93ab41f80
Anna Tarasova <atarasova@dcloud.tech>
2022-11-14 00:23:14 +0400

    fix: fixed HomeControllerTest

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -10,14 +10,20 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-@Disabled
 @WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private IngredientRepository ingredientRepository;
+
+    @MockBean
+    private OrderRepository orderRepository;
 
     @Test
     public void testHomePage() throws Exception {
