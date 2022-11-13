d4f0c2d21ea814644e2942861dfea9b58dc0c9a8
Anna Tarasova <atarasova@dcloud.tech>
2022-11-13 22:47:22 +0400

    feat: added JDBC logic

--- src/main/java/tacos/DesignTacoController.java
+++ src/main/java/tacos/DesignTacoController.java
@@ -1,6 +1,7 @@
 package tacos;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import tacos.Ingredient.Type;
 
 import javax.validation.Valid;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,20 +23,17 @@ import java.util.stream.Collectors;
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController (IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
+
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(ingredients::add);;
 
         Type[] types = Type.values();
         for (Type type : types) {
---
+++ src/main/java/tacos/IngredientRepository.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+
+    Optional<Ingredient> findById(String id);
+
+    Ingredient save(Ingredient ingredient);
+}
